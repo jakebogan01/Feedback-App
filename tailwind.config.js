3abae713f6c3091bd9037bb00e6dd61ad1aa6e9d
Jake Bogan <jakebogan01@gmail.com>
2023-06-01 02:30:02 -0400

    preped tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,20 @@ export default {
      content: ["./src/**/*.{html,js,svelte,ts}"],
      theme: {
           extend: {
-               //  fontFamily: {
-               //       spartan: ["League Spartan", "sans-serif"],
-               //  },
-               //  fontSize: {
-               //       13: "0.8125rem",
-               //       15: "0.9375rem",
-               //  },
-               //  lineHeight: {
-               //       15: "0.9375rem",
-               //       18: "1.125rem",
-               //       22: "1.375rem",
-               //       33: "2.0625rem",
-               //  },
-               //  screens: {
-               //       1440: "1440px",
-               //  },
+               fontFamily: {
+                    jost: ["Jost", "sans-serif"],
+               },
+               fontSize: {
+                    13: "0.8125rem",
+                    15: "0.9375rem",
+               },
+               lineHeight: {
+                    19: "1.1875rem",
+                    22: "1.375rem",
+               },
+               screens: {
+                    1440: "1440px",
+               },
           },
      },
      plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")],
