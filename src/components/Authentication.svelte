e76013712c978c71e2c1dc4e295bbd3714b5ac7f
Jake Bogan <jakebogan01@gmail.com>
2023-06-02 22:57:01 -0400

    udpate

--- src/components/Authentication.svelte
+++ src/components/Authentication.svelte
@@ -10,6 +10,7 @@
     let errors = {email: '', username: '', password: '', confirmPassword: '', noMatch: ''};
     let valid = false;
     let allUsers;
+    let openInfo = true;
 
 
     afterUpdate(()=> {
@@ -19,13 +20,15 @@
     const handleAuthentication = () => {
         valid = true;
         let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        let validEndings = ['@leadmarvels.com'];
+        const res = validEndings.some(endingStr => fields.email.trim().toLowerCase().endsWith(endingStr));
 
         if (fields.email == "") {
             valid = false;
         } else {
-            if (reg.test(fields.email.trim().toLowerCase()) == false) {
+            if (!res) {
                 valid = false;
-                errors.email = "Not a valid email";
+                errors.email = "Must have a Leadmarvels email";
             } else {
                 errors.email = "";
             }
@@ -42,7 +45,7 @@
             }
         }
 
-        if (fields.email !== "") {
+        if (fields.password === "") {
             valid = false;
         } else {
             let lowerCaseLetters = /[a-z]/g;
@@ -123,7 +126,7 @@
     }
 </script>
 
-<div class="flex flex-col items-center justify-center flex-1 p-6 text-white min-h-screen bg-[#011446]">
+<div class="relative flex flex-col items-center justify-center flex-1 p-6 text-white min-h-screen bg-[#011446]">
     {#if title === "register"}
         <form method="POST" action="?/register" enctype="multipart/form-data" use:enhance class="flex flex-col gap-5 w-[25rem] max-w-full mx-auto">
             <h1 class="text-center text-5xl mb-4 capitalize">{title}</h1>
@@ -262,6 +265,20 @@
                 <span on:keydown={()=>{}} on:click={()=>{title = 'register'}}>Register</span>
             </div>
         {/if}
+    </div>
+    <div class="hidden sm:block fixed left-4 bottom-4">
+        {#if openInfo}
+            <div class="absolute left-0 -top-20 bg-white rounded-[0.625rem] w-[25rem] p-4 text-gray-800">
+                <button on:click={()=>{openInfo = false}} type="button" class="absolute right-2 top-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clip-rule="evenodd" /></svg>                  
+                </button>
+                <p>This test app is only for Leadmarvel's employees.</p>
+                <p>Please do not use real passwords, thank you!</p>
+            </div>
+        {/if}
+        <div on:keydown={()=>{}} on:click={()=>{openInfo = true}} class="cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" /></svg>
+        </div>
     </div>
 </div>
  
